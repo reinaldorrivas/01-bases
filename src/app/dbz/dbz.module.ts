e6912e7569363e9524771f4da867717f8d21b0d9
Rene Rivas <rrivas@techinsights.com>
2022-10-09 16:41:28 -0400

    Settled DBZ module initial config

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AgregarComponent } from './agregar/agregar.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { PersonajesComponent } from './personajes/personajes.component';
+import { DbzService } from './services/dbz.service';
 
 @NgModule({
-  declarations: [
-    MainPageComponent
-  ],
-  imports: [CommonModule],
+  declarations: [MainPageComponent, PersonajesComponent, AgregarComponent],
+  exports: [MainPageComponent],
+  imports: [CommonModule, FormsModule],
+  providers: [DbzService],
 })
 export class DbzModule {}
