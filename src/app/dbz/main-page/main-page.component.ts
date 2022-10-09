6827c575f33c7d235911aa1e6508ca0de4eeb1be
Rene Rivas <rrivas@techinsights.com>
2022-10-09 16:38:21 -0400

    Main Page for DBZ Module

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.scss']
 })
-export class MainPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class MainPageComponent {
+  nuevoPersonaje: Personaje = {
+    nombre: 'Trunks',
+    poder: 14000,
+  };
 }
