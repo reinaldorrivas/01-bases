e8c8c8f8e6ac4cfc9717b9d0574e3d7fca7a81f1
Rene Rivas <rrivas@techinsights.com>
2022-10-09 16:40:57 -0400

    Add dbz main page component to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContadorModule } from './contador/contador.module';
+import { DbzModule } from './dbz/dbz.module';
 import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HeroesModule, ContadorModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HeroesModule,
+    ContadorModule,
+    DbzModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
